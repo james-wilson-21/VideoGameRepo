e64b05792b696db5eeb6b109861839eb51d9aaa9
James <treppiejames@hotmail.com>
2026-04-25 10:05:03 +1200

    Add project files.

---
+++ VideoGameRepo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideoGameRepo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
